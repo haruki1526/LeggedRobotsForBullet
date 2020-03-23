0c9fd8863fd9a41e4e5907b760dec517e4b1d467
HarukiTakamura <harki20452036@gmail.com>
2020-03-23 20:57:28 +0900

    Add Inverse Kinematics

--- bipedal/robot.py
+++ bipedal/robot.py
@@ -6,22 +6,21 @@ import time
 import tform as tf
 
 class Robot:
-    def __init__(self, robotPATH, StartPosition, startOrientation, maxForce, controlMode=pb.POSITION_CONTROL, planePATH="plane.urdf"):
+    def __init__(self, robotPATH, startPosition, startOrientation, maxForce, controlMode=pb.POSITION_CONTROL, planePATH="plane.urdf"):
         physicsClient = pb.connect(pb.GUI)
         pb.setAdditionalSearchPath(pybullet_data.getDataPath())
         pb.setGravity(0,0,-9.8)
         self._planeId = pb.loadURDF(planePATH)
-        self._robotId = pb.loadURDF(robotPATH,StartPosition, pb.getQuaternionFromEuler(startOrientation))
+        self._robotId = pb.loadURDF(robotPATH,startPosition, pb.getQuaternionFromEuler(startOrientation))
         self._controlMode = controlMode
         self.numJoint = pb.getNumJoints(self._robotId)
         self._jointIdList = [i for i in range(self.numJoint)]
 
         self.maxForce = maxForce
-        self._maxForceList = [maxForce for i in range(12)]
+        self._maxForceList = [maxForce for i in range(self.numJoint)]
 
         self._timeStep = 1./240.
-    
-        
+
 
     def getEuler(self):
         _, qua = pb.getBasePositionAndOrientation(self._robotId)
@@ -48,9 +47,15 @@ class Robot:
 
 class Bipedal(Robot):
 
-    def __init__(self, robotPATH="urdf/bipedal.urdf", planePATH="plane.urdf", StartPosition=[0,0,0.53], startOrientation=[0,0,0], maxForce = 9.0, controlMode=pb.POSITION_CONTROL):
-        super().__init__(robotPATH, StartPosition, startOrientation, maxForce, planePATH=planePATH)
+    def __init__(self, startPosition=[0,0,0.55], startOrientation=[0,0,0], maxForce=9.0, controlMode=pb.POSITION_CONTROL, robotPATH="urdf/bipedal.urdf", planePATH="plane.urdf"):
+        super().__init__(robotPATH=robotPATH, startPosition=startPosition, startOrientation=startOrientation, maxForce=maxForce, controlMode=controlMode, planePATH=planePATH)
 
+        self._lambda = 1.0
+        self._L1 = 0.18
+        self._L2 = 0.18
+        self.R = np.array([0,-0.065,-0.175])
+        self.L = np.array([0,0.065,-0.175])
+        self.LEG_DOF = 6
 
         self.RYawHipJointMotor = Motor(self._robotId, 0, mode=controlMode)
         self.RRollHipJointMotor = Motor(self._robotId, 1, mode=controlMode)
@@ -66,21 +71,34 @@ class Bipedal(Robot):
         self.LPitchAnkleJointMotor = Motor(self._robotId, 10, mode=controlMode)
         self.LRollAnkleJointMotor = Motor(self._robotId, 11, mode=controlMode)
 
-        self._rightIdList = [self.RYawHipJointMotor.Id, self.RRollHipJointMotor.Id, self.RPitchHipJointMotor.Id, 
+        self._jointIdListL = [self.RYawHipJointMotor.Id, self.RRollHipJointMotor.Id, self.RPitchHipJointMotor.Id, 
                             self.RKneeJointMotor.Id, self.RPitchAnkleJointMotor.Id, self.RRollAnkleJointMotor.Id]
 
-        self._leftIdList = [self.LYawHipJointMotor.Id, self.LRollHipJointMotor.Id, self.LPitchHipJointMotor.Id, 
+        self._jointIdListR = [self.LYawHipJointMotor.Id, self.LRollHipJointMotor.Id, self.LPitchHipJointMotor.Id, 
                             self.LKneeJointMotor.Id, self.LPitchAnkleJointMotor.Id, self.LRollAnkleJointMotor.Id]
+        self._maxForceListForLeg = [maxForce for i in range(self.LEG_DOF)]
 
-        self.COMtoL = np.array([0,0.065,-0.175])
-        self.COMtoR = np.array([0,-0.065,-0.175])
+        #joint axis matrix
+        self._a = np.array([[0,0,1], 
+                                [1,0,0],
+                                [0,1,0],
+                                [0,1,0],
+                                [0,1,0],
+                                [1,0,0]])
+
+        self._E = np.eye(3)
 
 
-    def setMotorPositionByArray(self, goalPositions):
-        pb.setJointMotorControlArray(self._robotId, jointIndices=self._jointIdList, controlMode=self._controlMode, forces=self._maxForceList, targetPositions=goalPositions)
+    def setMotorPositionByArray(self, targetJointPositions):
+        pb.setJointMotorControlArray(self._robotId, jointIndices=self._jointIdList, controlMode=self._controlMode, forces=self._maxForceList, targetPositions=targetJointPositions)
+
+    def setRightLegJointPositions(self, targetJointPositions):
+        pb.setJointMotorControlArray(self._robotId, jointIndices=self._jointIdListR, controlMode=self._controlMode, forces=self._maxForceListForLeg, targetPositions=targetJointPositions)
+
+    def setLeftLegJointPositions(self, targetJointPositions):
+        pb.setJointMotorControlArray(self._robotId, jointIndices=self._jointIdListL, controlMode=self._controlMode, forces=self._maxForceListForLeg, targetPositions=targetJointPositions)
 
     
-
 
     def torqueControlModeEnableForAll(self):
 
@@ -97,86 +115,85 @@ class Bipedal(Robot):
         self.LKneeJointMotor.torqueControlModeEnable()
         self.LPitchAnkleJointMotor.torqueControlModeEnable()
         self.LRollAnkleJointMotor.torqueControlModeEnable()
-
-
-    def setLegEndEffectorPositionAndOrientation(self, LegIdList, goalPosition, goalOrientation):
-        theta_vector = self.calculateIK(LegPosition, LegOrientation)
-        self.setMotorPositionByArray(goalPositions=theta_vector, jointIndices=LegIdList)
-
-    def getLegTrans(self, jointPositions):
+        
+        
+    def getLegTrans(self, jointPositions, leg):
         hipyaw = jointPositions[0]
-        hiproll = jointPositions[1] + np.pi/2
+        hiproll = jointPositions[1]
         hippitch = jointPositions[2]
         knee = jointPositions[3]
         anklepitch = jointPositions[4]
         ankleroll = jointPositions[5]
-
-
-        L1 = 0.18
-        L2 = 0.18
-        CoMtoL = np.array([0,0.1,-0.15])
-        T_w_0 = tf.getTransFromRp(tf.getRotationYaw(np.pi/2),CoMtoL)
-        zero_v = np.zeros(3)
-        I3 = np.eye(3)
-
-        T_w_0 = tf.getTransFromRp(tf.getRotationYaw(np.pi/2),CoMtoL)
-        T_0_1 = tf.getTransFromRp(tf.getRotationYaw(hipyaw),zero_v)
-        T_1_2 = tf.getTransFromRp(tf.getRotationRoll(np.pi/2),zero_v).dot(tf.getTransFromRp(tf.getRotationYaw(hiproll),zero_v))
-        T_2_3 = tf.getTransFromRp(tf.getRotationRoll(np.pi/2),zero_v).dot(tf.getTransFromRp(tf.getRotationYaw(hippitch),zero_v))
-        T_3_4 = tf.getTransFromRp(I3, np.array([-L1,0,0])).dot(tf.getTransFromRp(tf.getRotationYaw(knee),zero_v))
-        T_4_5 = tf.getTransFromRp(I3, np.array([-L2,0,0])).dot(tf.getTransFromRp(tf.getRotationYaw(anklepitch),zero_v))
-        T_5_6 = tf.getTransFromRp(tf.getRotationRoll(-np.pi/2), zero_v).dot(tf.getTransFromRp(tf.getRotationYaw(ankleroll),zero_v))
-
-        T_w_1 = T_w_0.dot(T_0_1)
-        T_w_2 = T_w_1.dot(T_1_2)
-        T_w_3 = T_w_2.dot(T_2_3)
-        T_w_4 = T_w_3.dot(T_3_4)
-        T_w_5 = T_w_4.dot(T_4_5)
-        T_w_6 = T_w_5.dot(T_5_6)
-
-        return T_w_0, T_w_1, T_w_2, T_w_3, T_w_4, T_w_5, T_w_6
-
-
-
-    def forwardKinematics(self, jointPositions):
-
-        L1 = 0.18
-        L2 = 0.18
-        T_w_0 = tf.getTransFromRp(tf.getRotationYaw(np.pi/2),self.COMtoL)
-        zero_v = np.zeros(3)
-        I3 = np.eye(3)
-
-        T_w_6 = getLegTrans(jointPositions)[6]
-
-
-        return tf.getRotationAndPositionFromT(T_w_6)
-
-
-
-
-
-    def jacobian(self, q):
-        Tw = self.getLegTrans(q)
-        a = np.array([0,0,1])
         zero_v = np.zeros(3)
 
-        R = [tf.getRotationFromT(Tw[i]) for i in range(len(Tw))]
-        p = [tf.getPositionFromT(Tw[i]) for i in range(len(Tw))]
+        T_0_1 = tf.getTransFromRp(tf.roadriguesEquation(self._E, self._a[0], hipyaw),leg)
+        T_0_2 = T_0_1.dot( tf.getTransFromRp(tf.roadriguesEquation(self._E, self._a[1], hiproll), zero_v))
+        T_0_3 = T_0_2.dot( tf.getTransFromRp(tf.roadriguesEquation(self._E, self._a[2], hippitch), zero_v))
+        T_0_4 = T_0_3.dot( tf.getTransFromRp(tf.roadriguesEquation(self._E, self._a[3], knee), [0,0,-self._L1]))
+        T_0_5 = T_0_4.dot( tf.getTransFromRp(tf.roadriguesEquation(self._E, self._a[4], anklepitch), [0,0,-self._L2]))
+        T_0_6 = T_0_5.dot( tf.getTransFromRp(tf.roadriguesEquation(self._E, self._a[5], ankleroll), zero_v))
+
+        return T_0_1, T_0_2, T_0_3, T_0_4, T_0_5, T_0_6
+        
+        
+        
+    def forwardKinematics(self, jointPositions, leg):
+
+        T_0_6 = self.getLegTrans(jointPositions, leg)[5]
+
+        return tf.getRotationAndPositionFromT(T_0_6)
+
+    def inverseKinematics(self, p_ref, omega_ref, leg):
+        q = self.getJointPositions(leg)
+        R, p = self.forwardKinematics(q, leg)
+        omega = np.array(tf.getRollPitchYawFromR(R))
+
+        dp = p_ref - p
+        domega = omega_ref - omega
+        dp_domega = np.append(dp,domega)
+
+        dq = self._lambda * np.linalg.inv(self.jacobian(q, leg)).dot(dp_domega)
 
 
-        wa = [R[i].dot(a) for i in range(len(R))]
+        return q+dq
 
+    def jacobian(self, q, leg):
+        T0 = self.getLegTrans(q, leg)
+        zero_v = np.zeros(3)
 
-        Jp = np.vstack((  np.hstack((np.cross( wa[i],(p[6]-p[i])), wa[i])) for i in range(1,len(wa)-1) ))
-        J = np.vstack(( Jp, np.hstack((zero_v, wa[6])) )).T
+        R = [tf.getRotationFromT(T0[i]) for i in range(len(T0))]
+        p = [tf.getPositionFromT(T0[i]) for i in range(len(T0))]
+
+        wa = [R[i].dot(self._a[i]) for i in range(len(R))]
+
+        Jp = np.vstack((  np.hstack((np.cross( wa[i],(p[5]-p[i])), wa[i])) for i in range(len(wa)-1)))
+        J = np.vstack(( Jp, np.hstack((zero_v, wa[5])) )).T
 
         return J
 
+    def getJointPositions(self, leg):
+        if np.sum(leg == self.R):
+            jointStates = pb.getJointStates(self._robotId, jointIndices=self._jointIdListR)
+            jointPositions = [jointStates[i][0] for i in range(len(jointStates))]
 
-    
+        elif np.sum(leg == self.L):
+            jointStates = pb.getJointStates(self._robotId, jointIndices=self._jointIdListL)
+            jointPositions = [jointStates[i][0] for i in range(len(jointStates))]
+        
+        else:
+            print("invalid parameter")
+            return -1
 
-    def getJointPositions(self):
-        jointStates = pb.getJointStates(self._robotId, jointIndices=self._jointIdList)
-        jointPositions = [jointStates[i][0] for i in range(self.numJoint)]
         return jointPositions
 
+    def IK(self):
+        currentR = self.getJointPositions(self.R)
+        currentL = self.getJointPositions(self.L)
+        current = currentR.append(currentL)
+        #posr = pb.calculateInverseKinematics(self._robotId, 5, [0.0, -0.065, -0.3], targetOrientation=[0,0,0,1], currentPositions=current)
+        posr = pb.calculateInverseKinematics(self._robotId, 5, [0.0, -0.065, -0.3], currentPositions=current)
+        #self.setRightLegJointPositions(posr[0:6])
+        #posl = pb.calculateInverseKinematics(self._robotId, 11, [0.0, 0.065, -0.3], targetOrientation=[0,0,0,1], currentPositions=current)
+        posl = pb.calculateInverseKinematics(self._robotId, 11, [0.0, 0.065, -0.3], currentPositions=current)
+        #self.setLeftLegJointPositions(posl[6:12])
+        return 0
--- bipedal/start.py
+++ bipedal/start.py
@@ -1,6 +1,7 @@
-
 import numpy as np
 from robot import Bipedal
+import tform as tf
+import time
 
 
 
@@ -8,19 +9,32 @@ from robot import Bipedal
 
 if __name__ == "__main__":
 
-    bipedal = Bipedal() #
+    bipedal = Bipedal() 
 
 
 
-    
-
-
-    jointPositions = bipedal.getJointPositions()
+    jointPositions = bipedal.getJointPositions(bipedal.R)
     print(jointPositions)
-    pos = [0.0 for i in range(12)]
+    tarPosR = [0.0, -0.065, -0.35]
+    tarPosL = [0.0, 0.065, -0.35]
+    targetRPY = [0.0, 0.0, 0.0]
+    posR = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    firstPosR = [0.0, 0.0, -0.6,  2*0.6, -0.6, 0.0]
+    firstPosL = [0.0, 0.0, -0.6,  2*0.6, -0.6, 0.0]
+    bipedal.setLeftLegJointPositions(firstPosL)
+    bipedal.setRightLegJointPositions(firstPosR)
+    bipedal.oneStep()
+    time.sleep(2)
     while(1):
-        bipedal.setMotorPositionByArray(goalPositions=pos)
-        jointPositions = bipedal.getJointPositions()
+        PosR = bipedal.inverseKinematics(tarPosR, targetRPY, bipedal.R)
+        PosL = bipedal.inverseKinematics(tarPosL, targetRPY, bipedal.L)
+        bipedal.setLeftLegJointPositions(PosL)
+        bipedal.setRightLegJointPositions(PosR)
+        R, p = bipedal.forwardKinematics(jointPositions=bipedal.getJointPositions(bipedal.R), leg=bipedal.R)
+        omega = tf.getRollPitchYawFromR(R)
+        print("R=",omega)
+
+
         bipedal.oneStep()
 
 
