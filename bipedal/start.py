9c618e7238272f217c76f0d0db33176f488cd18d
HarukiTakamura <harki20452036@gmail.com>
2020-03-25 23:57:19 +0900

    Clean

--- bipedal/start.py
+++ bipedal/start.py
@@ -11,20 +11,13 @@ if __name__ == "__main__":
 
     bipedal = Bipedal() 
 
+    initializeTime = 1.0
+    bipedal.positionInitialize(initializeTime=initializeTime)
 
 
-    jointPositions = bipedal.getJointPositions(bipedal.R)
-    print(jointPositions)
-    tarPosR = [0.0, -0.065, -0.35]
-    tarPosL = [0.0, 0.065, -0.35]
+    tarPosR = [0.0, -0.065, -0.4]
+    tarPosL = [0.0, 0.065, -0.4]
     targetRPY = [0.0, 0.0, 0.0]
-    posR = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    startJointPosR = [0.0, 0.0, -0.6,  2*0.6, -0.6, 0.0]
-    startJointPosL = [0.0, 0.0, -0.6,  2*0.6, -0.6, 0.0]
-    bipedal.setLeftLegJointPositions(startJointPosL)
-    bipedal.setRightLegJointPositions(startJointPosR)
-    bipedal.oneStep()
-    time.sleep(2)
     while(1):
         PosR = bipedal.inverseKinematics(tarPosR, targetRPY, bipedal.R)
         PosL = bipedal.inverseKinematics(tarPosL, targetRPY, bipedal.L)
@@ -32,7 +25,7 @@ if __name__ == "__main__":
         bipedal.setRightLegJointPositions(PosR)
         #R, p = bipedal.forwardKinematics(jointPositions=bipedal.getJointPositions(bipedal.R), leg=bipedal.R)
         #omega = tf.getRollPitchYawFromR(R)
-        #print("omega=",omega)
+        print("pos=", PosR)
 
 
         bipedal.oneStep()
